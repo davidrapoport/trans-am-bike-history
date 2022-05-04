5e5e4da2ac984460c41f63645fef111bbe5ccb5e
David Rapoport <drapoport847@gmail.com>
2022-05-04 16:23:35 -0600

    mess around with typeface, add a new favicon

--- bike-map/src/footer.js
+++ bike-map/src/footer.js
@@ -18,8 +18,8 @@ function Footer() {
             a night in Elkhorn City, KY where the mayor, Mike Taylor, very
             kindly let me spend the night in City Hall to avoid the rain and the
             heat (and to sit in on a very enthusiastic city council meeting). In
-            the morning I ran off to grab some breakfast at{" "}
-            <a href="https://goo.gl/maps/aAkR6HxFzMeMB5YcA">RFG cafe*</a>. The
+            the morning I ran off to grab some breakfast at the{" "}
+            <a href="https://goo.gl/maps/aAkR6HxFzMeMB5YcA">RFG cafe</a>*. The
             lady who ran the shop had a map up on the wall with pins marking
             where all her visitors were from. There was one pin up from Sweden
             and she told me that that came from a 60ish year old guy who had
@@ -41,27 +41,23 @@ function Footer() {
           <p className="footnote">
             *Yes, these are affiliate links. Duh, it's 2022.
           </p>
-          <br />
-          <br />
           <div>
             <p>
               Some of the questions I'd like to ask people (lemme know if you
               think up any good ones)
             </p>
             <ul>
-              <li>
-                What was it like growing up here? How has life changed in the
-                past 30 years?
-              </li>
+              <li>What was it like growing up here?</li>
+              <li> How has life changed in the past 30 years?</li>
               <li>
                 What do you hope the world/your town/state will look like in 30
                 years?
               </li>
+              <li>Who is your most interesting neighbor? </li>
               <li>
-                Who is your most interesting neighbor? When did you/your family
-                move here?
+                When did you/your family move here? What brought you/them out
+                here?
               </li>
-              <li>What brought you/them out here?</li>
               <li>
                 What's your favorite memory of summer/winter/fall/spring?{" "}
               </li>
@@ -70,19 +66,24 @@ function Footer() {
           </div>
         </div>
         <div className="thanks">
-          <h3>With many thanks to:</h3>
+          <h3>With thanks to:</h3>
           <ul>
-            <li>Me</li>
-            <li>Myself</li>
-            <li>I</li>
-            <li>Literally just me</li>
-            <li>Not you</li>
+            <li>Everyone who shared their story</li>
+            <li>Leaflet</li>
+            <li>React-Leaflet</li>
+            <li>
+              Priscille for being{" "}
+              <a href="https://microstoriefr.weebly.com/storymap.html">
+                an inspiration
+              </a>
+            </li>
           </ul>
         </div>
         <div className="login">
           <h5>
-            Are you David? Are you trying to upload a story? Click here.
-            Otherwise, please don't.
+            Are you David? Are you huddled in a McDonald's bathroom leeching
+            power and trying to upload a story? Click here. Otherwise, please
+            don't.
           </h5>
           {link}
         </div>
--- bike-map/src/header.js
+++ bike-map/src/header.js
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <div className="header">
-      <h1>A journey along the Northern US</h1>
+      <h1>A Journey Through the Northern US</h1>
     </div>
   );
 }
